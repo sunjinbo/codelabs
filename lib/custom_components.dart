0a69906cae8188cdd864b5a71c636fb1bddbceb0
sunjinbo <sunjinbo@outlook.com>
2021-11-05 19:01:04 +0800

    watermark

--- lib/custom_components.dart
+++ lib/custom_components.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'dart:ui' as ui;
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -275,4 +276,100 @@ class _GradientCircularProgressPainter extends CustomPainter {
   @override
   bool shouldRepaint(CustomPainter oldDelegate) => true;
 
+}
+
+class WaterMark extends StatefulWidget {
+  final WaterMarkPainter painter;
+
+  final ImageRepeat repeat;
+
+  WaterMark({
+    Key? key,
+    this.repeat = ImageRepeat.repeat,
+    required this.painter,
+  }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _WaterMarkState();
+}
+
+class _WaterMarkState extends State<WaterMark> {
+  late Future<MemoryImage> _memoryImageFuture;
+
+  @override
+  void initState() {
+    _memoryImageFuture = _getWaterMarkImage();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand(
+      child: FutureBuilder(
+        future: _memoryImageFuture,
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState != ConnectionState.done) {
+            // 如果单元水印还没有绘制好先返回一个空的Container
+            return Container();
+          } else {
+            // 如果单元水印已经绘制好，则渲染水印
+            return DecoratedBox(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: snapshot.data,
+                  repeat: widget.repeat,
+                  alignment: Alignment.topLeft
+                )
+              ),
+            );
+          }
+        }),
+      );
+  }
+
+  @override
+  void didUpdateWidget(WaterMark oldWidget) {
+    // 如果画笔发生变化（类型or配置）则重新绘制水印
+    if (widget.painter.runtimeType != oldWidget.painter.runtimeType
+      || widget.painter.shouldRepaint(oldWidget.painter)) {
+      // 先释放之前的缓存
+      _memoryImageFuture.then((value) => value.evict());
+      // 重新绘制并缓存
+      _memoryImageFuture = _getWaterMarkImage();
+    }
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void dispose() {
+    // 释放图片缓存
+    _memoryImageFuture.then((value) => value.evict());
+    super.dispose();
+  }
+
+  Future<MemoryImage> _getWaterMarkImage() async {
+    // 创建一个 Canvas 进行离屏绘制，细节和原理请查看本书后面关于Flutter绘制原理相关章节
+    final recorder = ui.PictureRecorder();
+    final canvas = Canvas(recorder);
+    // 绘制单元水印并获取其大小
+    final Size size = widget.painter.paintUnit(canvas, 0);
+    final picture = recorder.endRecording();
+    //将单元水印导为图片并缓存起来
+    final img = await picture.toImage(size.width.ceil(), size.height.ceil());
+    final byteData = await img.toByteData(format: ui.ImageByteFormat.png);
+    final pngBytes = byteData!.buffer.asUint8List();
+    return MemoryImage(pngBytes);
+  }
+}
+
+abstract class WaterMarkPainter extends CustomPainter {
+  /// 绘制"单元水印"，完整的水印是由单元水印重复平铺组成,返回值为"单元水印"占用空间的大小。
+  /// [devicePixelRatio]: 因为最终要将绘制内容保存为图片，所以在绘制时需要根据屏幕的
+  /// DPR来放大，以防止失真
+  Size paintUnit(Canvas canvas, double devicePixelRatio);
+
+  @override
+  bool shouldRepaint(covariant WaterMarkPainter oldDelegate) {
+    return true;
+  }
 }
